a5ecf4146cc0132b73a86938dcd336d88554e0bc
mnomanfarooq <49403691+mnomanfarooq@users.noreply.github.com>
2020-07-15 18:07:05 +0500

    Update Route.js

--- src/Route.js
+++ src/Route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
 import About from './Components/About';
 import Product from './Components/Product';
@@ -12,19 +12,19 @@ import Formal from './Components/Formal';
 function RouteConfig() {
     return (
         <>
-            <Router>
+            <Routes>
                 <Header />
                 
-                    <Route path="/" element={Home} />
-                    <Route path="/about" element={About} />
-                    <Route path="/product" element={Product} />
-                    <Route path="/product/:id" element={ProductItems} />
-                    <Route path="/formal" element={Formal} />
-                    <Route path="/formal/:id" element={FormalShoes} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/product/:id" element={<ProductItems />} />
+                    <Route path="/formal" element={<Formal />} />
+                    <Route path="/formal/:id" element={<FormalShoes />} />
                     <Route path="*" element={() => <h2>Not Found</h2>} />
                 
                 <Footer />
-            </Router>
+            </Routes>
 
         </>
     )
