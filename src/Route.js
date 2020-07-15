6c2f15e11e23ebe40430b57dfae48fd5511c26e4
mnomanfarooq <49403691+mnomanfarooq@users.noreply.github.com>
2020-07-15 15:41:08 +0500

    Update Route.js

--- src/Route.js
+++ src/Route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Components/Home';
 import About from './Components/About';
 import Product from './Components/Product';
