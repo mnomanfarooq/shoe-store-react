33e3ca880b0b30a31a601c531657d4557d0e37ee
mnomanfarooq <49403691+mnomanfarooq@users.noreply.github.com>
2020-07-15 15:43:24 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './../App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Slider from './Slider'
 
 const useStyles = makeStyles((theme) => ({
